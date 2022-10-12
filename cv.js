3ca2fe7a14ba834a24faafbfec6c7724a68f22d6
Ali Elsayed <ali-hisham@hotmail.com>
2022-10-12 15:21:09 +0300

    mobile compatibility

--- cv.js
+++ cv.js
@@ -1,13 +1,11 @@
-AOS.init({offset:250,duration:1200});
-var icon=document.getElementById("night");
+AOS.init({ offset: 250, duration: 1200 });
 
-icon.onclick=function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src="images/sun.png";
-    }else{
-        icon.src="images/NightMode.png";
-    }
-}
-
-
+var icon = document.getElementById("night");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/NightMode.png";
+  }
+};
