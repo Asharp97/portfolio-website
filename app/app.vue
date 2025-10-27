9cd1a8e50102af9197264ff123678768882465f7
asharp97 <Ali-hisham@hotmail.com>
2025-10-27 14:08:39 +0300

    feat: integrate Vercel Analytics for enhanced performance tracking

--- app/app.vue
+++ app/app.vue
@@ -2,10 +2,14 @@
   <!-- <Lenis root> -->
   <Noise />
   <NuxtLayout>
+    <Analytics/>
     <NuxtPage />
   </NuxtLayout>
   <!-- </Lenis> -->
 </template>
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
 <style>
 html {
   scroll-behavior: smooth;
